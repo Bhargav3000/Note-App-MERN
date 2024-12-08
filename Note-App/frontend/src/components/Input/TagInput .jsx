f5d432bf18259357460c3cde887c04ec9006e2ec
Bhargav3000 <bvenkatabhargav2003@gmail.com>
2024-12-08 20:37:14 +0530

    Second commit

--- Note-App/frontend/src/components/Input/TagInput .jsx
+++ Note-App/frontend/src/components/Input/TagInput .jsx
@@ -1,31 +1,29 @@
-import React, { useState } from "react"
-import { MdAdd, MdClose } from "react-icons/md"
+import React, { useState } from "react";
+import { MdAdd, MdClose } from "react-icons/md";
 
 const TagInput = ({ tags, setTags }) => {
-  const [inputValue, setInputValue] = useState("")
-
-  //   console.log(inputValue)
+  const [inputValue, setInputValue] = useState("");
 
   const handleInputChange = (e) => {
-    setInputValue(e.target.value)
-  }
+    setInputValue(e.target.value);
+  };
 
   const addNewTag = () => {
     if (inputValue.trim() !== "") {
-      setTags([...tags, inputValue.trim()])
-      setInputValue("")
+      setTags([...tags, inputValue.trim()]);
+      setInputValue("");
     }
-  }
+  };
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      addNewTag()
+      addNewTag();
     }
-  }
+  };
 
   const handleRemoveTag = (tagToRemove) => {
-    setTags(tags.filter((tag) => tag !== tagToRemove))
-  }
+    setTags(tags.filter((tag) => tag !== tagToRemove));
+  };
   return (
     <div>
       {tags?.length > 0 && (
@@ -38,7 +36,7 @@ const TagInput = ({ tags, setTags }) => {
               # {tag}
               <button
                 onClick={() => {
-                  handleRemoveTag(tag)
+                  handleRemoveTag(tag);
                 }}
               >
                 <MdClose />
@@ -61,14 +59,14 @@ const TagInput = ({ tags, setTags }) => {
         <button
           className="w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700"
           onClick={() => {
-            addNewTag()
+            addNewTag();
           }}
         >
           <MdAdd className="text-2xl text-blue-700 hover:text-white" />
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TagInput
+export default TagInput;
